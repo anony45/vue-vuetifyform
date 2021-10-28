ac401c2a5a97eaf9bfe5a5cd47f9aef7034df89d
ashuka <shamageto@gmail.com>
2021-10-28 14:49:16 +0300

    added registration form fields

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+  <v-system-bar app dense color="orange " class="text--white">  COAST PEOPLE'S FORUM</v-system-bar><br><br>
+  <v-app-bar  prominent color="white" >
+   <v-img class="shrink mr-lg-1" src="./assets/logo.png" height="30px" width="70px"></v-img>
+  
+  <v-spacer></v-spacer>
+  <v-btn text outlined color="orange">LOGIN</v-btn>
+  </v-app-bar>
+ 
+  
+    <v-main>
+      <router-view/>
+      <v-main color="orange" text-color="primary">
+      
+      
+      
+      </v-main>
+    </v-main>
+    <v-footer dark cardless>
+   
+          <span >© 2021 Coast People's Forum</span>
+          <v-spacer></v-spacer>
+          <span>Powered by Alumnipad</span>
+  
+
+    
+    
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app>
+    <v-main>
+    <v-main>
+    <v-form >
+    <v-row class="center" justify="center">
+    <v-col>
+    <v-col cols="12" sm="8">
+
+      <v-text-field v-model="name" label="Name" required outlined dense color="orange">
+      </v-text-field>
+       <v-text-field v-model="email" label="Email" required outlined dense  color="orange">
+      </v-text-field>
+
+      <v-row >
+        <v-col cols="12" sm="4">
+           <v-select v-model="countrycode" label="Country Code" outlined dense color="orange" ></v-select>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <v-text-field v-model="phonenumber" label="Phone number" required outlined dense color="orange">
+          </v-text-field>
+         </v-col>
+      </v-row>
+      
+       
+       <v-text-field v-model="profession" label="Profession" required outlined dense color="orange">
+      </v-text-field>
+       <v-text-field v-model="gender" label="Gender" required outlined dense color="orange">
+      </v-text-field>
+       <v-text-field v-model="country" label="Country" required outlined dense color="orange">
+      </v-text-field>
+       <v-text-field v-model="password" label="Password" required outlined dense color="orange">
+      </v-text-field>
+       <v-text-field v-model="confirmpassworrd" label="Confirm Password" required outlined dense color="orange">
+      </v-text-field>
+
+        <v-btn color="orange" >
+      REGISTER
+    </v-btn>
+    
+    </v-col>
+    </v-col>
+
+    </v-row>
+    
+    
+    
+    
+    </v-form>
+    
+    </v-main>
+    </v-main>
+  
+  
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'register',
+
+    data: () => ({
+
+      name:"",
+      email:"",
+      countrycode:"",
+      phonenumber:"",
+      profession:"",
+      gender:"",
+      country:"",
+      password:"",
+      confirmpassword:"",
+
+     
+    }),
+  }
+</script>
